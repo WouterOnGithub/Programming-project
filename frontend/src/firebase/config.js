6cd712954475c498c423944afb5c553678b7c587
Yas3tech <eddouksy@gmail.com>
2025-06-11 11:06:37 +0200

    firebase

---
+++ frontend/src/firebase/config.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0bYG2yBCgap3Ov7AT_Vp9h1RSTYOxx-8",
+  authDomain: "ehbdata.firebaseapp.com",
+  projectId: "ehbdata",
+  storageBucket: "ehbdata.firebasestorage.app",
+  messagingSenderId: "970410423416",
+  appId: "1:970410423416:web:a32b39ce0b554daae4698d",
+  measurementId: "G-3NM1W6ZQWG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Initialize Auth
+const auth = getAuth(app);
+
+// Initialize Analytics
+const analytics = getAnalytics(app);
+
+// Initialize Storage
+const storage = getStorage(app);
+
+export { db, auth, analytics, storage }; 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './firebase/config'; // Import Firebase configuration
 
 createApp(App).use(router).mount('#app');
