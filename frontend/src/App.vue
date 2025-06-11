d2992e3e863b05714f0f814173de19c15baa8033
imadbvi <imad.ben.ali.idrissi@student.ehb.be>
2025-06-11 09:55:30 +0200

    footer vast zetten

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,3 +20,32 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 </script>
+
+
+<style scoped>
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.main-content {
+  flex: 1;
+}
+
+.footer {
+  background-color: #e0e0e0;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #ccc;
+  border-radius: 6px 6px 0 0;
+}
+
+</style>
